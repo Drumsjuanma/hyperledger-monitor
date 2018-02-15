b145d145592baa758fc49a63c7a63d1e999fb341
root <root@LKXAUBU01.LACAIXA.COM>
2018-02-15 14:49:53 +0000

    binarios añadidos

--- installers/fabric.sh
+++ installers/fabric.sh
@@ -7,6 +7,10 @@ mkdir -p $GOPATH/src/github.com/hyperledger/
 cd $GOPATH/src/github.com/hyperledger/
 git clone https://github.com/hyperledger/fabric.git
 cd fabric
+
+echo 'export PATH=$PATH:$GOPATH/src/github.com/hyperledger/fabric/build/bin/' >> $HOME/.bashrc
+cp build/bin/* /bin
+
 make
 
 cd examples/e2e_cli/ && ./network_setup.sh up
